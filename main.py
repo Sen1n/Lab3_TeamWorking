31ab84c72857af83b4d11b5699a1e1a693bfb5b8
Oleksandr <shchursasha11@gmail.com>
2024-02-11 20:22:42 +0200

    Added prime number check func

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from utils import fac
+from utils import fac, is_prime
 
 
 def main():
     n = 3
     print(f"Факторіал числа {n} дорівнює {fac(n)}")
+
+
+print(is_prime(1))
 
 
 if __name__ == "__main__":
