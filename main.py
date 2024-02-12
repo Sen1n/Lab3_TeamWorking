59e147a100ae1eabfc55d6c3cc6abef5aa37a016
Dmytro Chuchman <dmytrochuchman@knu.ua>
2024-02-12 09:25:57 +0200

    Додавання файлів utils.py та main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from utils import fac, is_prime, is_lucky
+from utils import fac, is_prime, is_lucky, nsd
 
 
 def main():
@@ -9,7 +9,7 @@ def main():
 print(is_prime(1))
 print(is_lucky(37))
 print(is_lucky(33))
-
+print(nsd(23, 43))
 
 if __name__ == "__main__":
     main()
